c4d7d639698bb751eff0ecf5f8d05188a0386fc1
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:03 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "route_filters" {
-  description = "All route_filter resources"
-  value       = azurerm_route_filter.route_filters
-}
 output "route_filters_location" {
-  description = "List of location values across all route_filters"
-  value       = [for k, v in azurerm_route_filter.route_filters : v.location]
+  description = "Map of location values across all route_filters, keyed the same as var.route_filters"
+  value       = { for k, v in azurerm_route_filter.route_filters : k => v.location }
 }
 output "route_filters_name" {
-  description = "List of name values across all route_filters"
-  value       = [for k, v in azurerm_route_filter.route_filters : v.name]
+  description = "Map of name values across all route_filters, keyed the same as var.route_filters"
+  value       = { for k, v in azurerm_route_filter.route_filters : k => v.name }
 }
 output "route_filters_resource_group_name" {
-  description = "List of resource_group_name values across all route_filters"
-  value       = [for k, v in azurerm_route_filter.route_filters : v.resource_group_name]
+  description = "Map of resource_group_name values across all route_filters, keyed the same as var.route_filters"
+  value       = { for k, v in azurerm_route_filter.route_filters : k => v.resource_group_name }
 }
 output "route_filters_rule" {
-  description = "List of rule values across all route_filters"
-  value       = [for k, v in azurerm_route_filter.route_filters : v.rule]
+  description = "Map of rule values across all route_filters, keyed the same as var.route_filters"
+  value       = { for k, v in azurerm_route_filter.route_filters : k => v.rule }
 }
 output "route_filters_tags" {
-  description = "List of tags values across all route_filters"
-  value       = [for k, v in azurerm_route_filter.route_filters : v.tags]
+  description = "Map of tags values across all route_filters, keyed the same as var.route_filters"
+  value       = { for k, v in azurerm_route_filter.route_filters : k => v.tags }
 }
 
